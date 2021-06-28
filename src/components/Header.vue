08e876c04b1b89cb3061f39d0a56d391667b022a
AJ-Wi <tropaguararia28@gmail.com>
2021-06-28 10:33:23 -0500

    creado cominezo del header

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <header id="site-header">
+    <div id="nav">
+      <router-link to="/">Dashboard</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
+</template>
