0bc336e15eeb14d941564b70f85ed1466521e46d
AJ-Wi <tropaguararia28@gmail.com>
2021-07-02 10:58:36 -0500

    creando header mobil

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,50 @@
 <template>
   <header id="site-header">
-    <div id="nav">
-      <router-link to="/">Dashboard</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav id="site-header_nav">
+      <h1>
+        <router-link to="/">Recipe Medico</router-link>
+      </h1>
+      <BtnMenu />
+    </nav>
   </header>
 </template>
+
+<script>
+import BtnMenu from "@/components/BtnMenu.vue";
+export default {
+  components: {
+    BtnMenu,
+  },
+};
+</script>
+
+<style>
+#site-header {
+  background-color: var(--primary-color);
+  padding: 0 20px;
+  display: flex;
+  position: sticky;
+  top: 0;
+}
+
+#site-header::after {
+  content: "";
+  position: absolute;
+  top: calc(60px - 4px);
+  left: 0;
+  width: 100%;
+  height: 4px;
+  background: linear-gradient(
+    to right,
+    var(--primary-color),
+    var(--second-color)
+  );
+}
+
+#site-header_nav {
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 40px;
+  align-content: center;
+}
+</style>
