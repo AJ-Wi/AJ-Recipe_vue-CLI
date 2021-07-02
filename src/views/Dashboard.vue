cfd2830bca3750fe7e1828c8bbdbacba11a16c0c
AJ-Wi <tropaguararia28@gmail.com>
2021-07-02 12:49:53 -0500

    agregados botones principales

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="dashboard">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="dashboard">
+    <NavButton :rute="require(`@/assets/paciente.png`)" title="Consulta" />
+    <NavButton :rute="require(`@/assets/historial.png`)" title="Historias" />
+    <NavButton
+      :rute="require(`@/assets/enfermedades.png`)"
+      title="Enfermedades"
+    />
+    <NavButton :rute="require(`@/assets/dietas.png`)" title="Dietas" />
+    <NavButton :rute="require(`@/assets/terapias.png`)" title="Terapias" />
+    <NavButton :rute="require(`@/assets/consejos.png`)" title="Consejos" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import NavButton from "@/components/NavButton.vue";
 
 export default {
   name: "Dashboard",
   components: {
-    HelloWorld,
+    NavButton,
   },
 };
 </script>
+
+<style>
+#dashboard {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 15px;
+  gap: 15px;
+}
+</style>
