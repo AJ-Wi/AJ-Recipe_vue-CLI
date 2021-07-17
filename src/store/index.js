b2cee4618917bfbf29fb88872e0fc5125ed12352
AJ-Wi <tropaguararia28@gmail.com>
2021-07-17 12:47:36 -0500

    utilizando tienda de datos y agregando ruta

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,26 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state() {
+    return {
+      navBarOpen: false,
+    };
+  },
+  mutations: {
+    isOpen(state) {
+      state.navBarOpen = !state.navBarOpen;
+    },
+    isClose(state) {
+      state.navBarOpen = false;
+    },
+  },
+  actions: {
+    toggleNavBar({ commit }) {
+      commit("isOpen");
+    },
+    closeNavBar({ commit }) {
+      commit("isClose");
+    },
+  },
   modules: {},
 });
