6bf76263d98c50f85c67c998c3bc507aa67dc19e
AJ-Wi <tropaguararia28@gmail.com>
2021-07-17 12:48:33 -0500

    reaciendo el header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,36 +1,47 @@
 <template>
-  <header id="site-header">
-    <nav id="site-header_nav">
-      <h1>
+  <header class="site-header">
+    <nav class="site-navbar">
+      <h1 class="site-title">
         <router-link to="/">Recipe Medico</router-link>
       </h1>
-      <BtnMenu />
+      <div
+        @click="isOpen"
+        class="site-hamburger"
+        :class="{ 'is-open': navBarOpen }"
+      >
+        <i class="fas fa-bars" id="menu-bar"></i>
+      </div>
     </nav>
   </header>
 </template>
 
 <script>
-import BtnMenu from "@/components/BtnMenu.vue";
+import { mapActions, mapState } from "vuex";
+
 export default {
-  components: {
-    BtnMenu,
+  computed: {
+    ...mapState(["navBarOpen"]),
+  },
+  methods: {
+    ...mapActions(["toggleNavBar"]),
+    isOpen() {
+      this.toggleNavBar();
+    },
   },
 };
 </script>
 
 <style>
-#site-header {
+.site-header {
   background-color: var(--primary-color);
-  padding: 0 15px;
-  display: flex;
   position: sticky;
   top: 0;
 }
 
-#site-header::after {
+.site-header::after {
   content: "";
   position: absolute;
-  top: calc(60px - 4px);
+  top: calc(var(--height-header) - 4px);
   left: 0;
   width: 100%;
   height: 4px;
@@ -41,10 +52,57 @@ export default {
   );
 }
 
-#site-header_nav {
-  width: 100%;
+.site-navbar {
   display: grid;
-  grid-template-columns: 1fr 40px;
-  align-content: center;
+  height: var(--height-header);
+  grid-template-columns: repeat(8, 1fr);
+}
+
+.site-title {
+  grid-column: 1 / 3;
+  min-width: 150px;
+  font-size: 1.3rem;
+  align-self: center;
+  margin-left: 1rem;
+}
+
+.site-hamburger {
+  grid-column: 8 / 9;
+  justify-self: flex-end;
+  width: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--second-color);
+  font-size: 2em;
+  cursor: pointer;
+  z-index: 1;
+  transition: all 0.5s;
+}
+
+@media (min-width: 738px) {
+  .site-title {
+    font-size: 1.5rem;
+  }
+}
+
+@media (min-width: 959px) {
+  .site-title {
+    grid-column: 4 / 6;
+    justify-self: center;
+  }
+
+  .site-hamburger {
+    grid-column: 1 / 3;
+    grid-row: 1 / 2;
+    justify-self: flex-start;
+    border-right: 1px solid var(--second-color);
+  }
+
+  .site-hamburger.is-open {
+    width: 220px;
+    justify-content: flex-end;
+    padding-right: 1rem;
+  }
 }
 </style>
