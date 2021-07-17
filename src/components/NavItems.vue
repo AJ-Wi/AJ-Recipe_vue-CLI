7cb32d82d5ddb8db4272e5e572f7c34aed33beba
AJ-Wi <tropaguararia28@gmail.com>
2021-07-17 12:49:15 -0500

    creando botones para la navegacion

---
+++ src/components/NavItems.vue
@@ -0,0 +1,40 @@
+<template>
+  <router-link :to="link" class="nav-item">
+    <img :src="rute" :alt="title" class="nav-img" />
+    <h3 class="nav-title">{{ title }}</h3>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: String,
+      default: "/",
+    },
+    rute: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style>
+.nav-item {
+  display: grid;
+  height: 60px;
+  align-content: center;
+}
+
+.nav-item .nav-img {
+  width: 70%;
+  justify-self: center;
+}
+
+.nav-item .nav-title {
+  align-self: center;
+}
+</style>
